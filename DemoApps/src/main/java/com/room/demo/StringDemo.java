79fbbc2a1105464310982fabb07d166a2be6005f
lenovo <lenovo@DESKTOP-SAQ7TUL>
2019-08-13 23:22:50 +0530

    D2 Commit

--- DemoApps/src/main/java/com/room/demo/StringDemo.java
+++ DemoApps/src/main/java/com/room/demo/StringDemo.java
@@ -4,7 +4,7 @@ public class StringDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s[] = {"Ramu","Raja","Raju","Gopi","Reddeppa","Reddy"};
+		String s[] = {"Ramu","Raja","Raju","Gopi","Reddeppa","Reddy","rrr"};
 		for(String name:s) {
 			System.out.println(s);
 		}
